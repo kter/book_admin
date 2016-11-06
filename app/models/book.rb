27ea4833ce92b4330da15b056a8f72290868ac3a
kter <me@kter.jp>
2016-11-06 23:33:28 +0900

    バリデーションを追加 ・本の名前は必ず入っていて欲しい ・本の名前は15文字までに収まっていて欲しい ・本の価格は0以上の値になっていて欲しい ・独自バリデーションを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,13 @@ class Book < ActiveRecord::Base
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 15 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
 end
